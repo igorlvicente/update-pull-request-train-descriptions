c30957ec6fa94e65ab1f7d2c01eeb62c4d8e07ca
Igor Lemos Vicente <igor.vicente@railsware.com>
2022-04-01 08:33:26 -0300

    Changes main.rb to start from master

--- main.rb
+++ main.rb
@@ -2,9 +2,15 @@
 
 require 'json'
 
-github_cli_output = `gh pr list --json baseRefName,headRefName,url`
+main_branch = if ARGV[1]
+                ARGV[1]
+              else
+                puts "Branch not specified. Using master"
+                'master'
+              end
+github_cli_output = `gh pr list -L 999 --json baseRefName,headRefName,url`
 json = JSON.parse(github_cli_output)
-initial_object = Hash.new { |hash, key| hash[key] = { 'dependents' => [] } }
+initial_object = Hash.new { |hash, key| hash[key] = {'dependents' => []} }
 dependency_hash = json.each_with_object(initial_object) do |pull_request_data, object|
   head_branch = pull_request_data['headRefName']
   base_branch = pull_request_data['baseRefName']
@@ -12,37 +18,38 @@ dependency_hash = json.each_with_object(initial_object) do |pull_request_data, o
   object[base_branch]['dependents'].push(head_branch)
   object
 end
-pull_requests_with_dependency = dependency_hash#.select { |_key, value| value['dependents'].size.positive? }
+pull_requests_with_dependency = dependency_hash #.select { |_key, value| value['dependents'].size.positive? }
 
 class DepthFirstSearch
-  attr_accessor :nodes
+  attr_accessor :nodes, :visited
 
   def initialize(nodes)
     @nodes = nodes
+    @visited = Hash.new { |hash, key| hash[key] = {} }
   end
 
-  def execute
-    search('dev', 0)
+  def execute(starting_node)
+    search(starting_node, 0)
     # nodes.keys.each do |branch|
     #   search(branch, 0) if can_enter_node?(branch)
     # end
   end
 
-  def search(node_name, level)
-    puts "#{'    ' * level}- [#{node_name}](#{nodes[node_name]['url']})" if nodes[node_name]['dependents'].size.positive? || level > 1
-    nodes[node_name][:dfs_in] = true
-    nodes[node_name]['dependents'].each do |linked_node|
-      raise "Redundant dependency between #{node_name} AND #{linked_node}" if nodes[linked_node][:dfs_in]
+  def search(current_node, level)
+    puts "#{'    ' * level}- [#{current_node}](#{nodes[current_node]['url']})" if nodes[current_node]['dependents'].size.positive? || level > 1
+    visited[current_node][:dfs_in] = true
+    nodes[current_node]['dependents'].each do |dependent_node|
+      raise "Redundant dependency between #{current_node} AND #{dependent_node}" if visited[dependent_node][:dfs_in]
 
-      search(linked_node, level + 1) if can_enter_node?(linked_node)
+      search(dependent_node, level + 1) if can_enter_node?(dependent_node)
     end
-    nodes[node_name][:dfs_out] = true
-    nodes[node_name][:dfs_in] = false
+    visited[current_node][:dfs_out] = true
+    visited[current_node][:dfs_in] = false
   end
 
   def can_enter_node?(node)
-    !nodes[node][:dfs_out]
+    !visited[node][:dfs_out]
   end
 end
 
-DepthFirstSearch.new(pull_requests_with_dependency).execute
+DepthFirstSearch.new(pull_requests_with_dependency).execute(main_branch)
